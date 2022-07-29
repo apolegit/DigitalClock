be89a3983bb07fd83bcdd6654a7afff57ead563c
Kenneth Apole <kennethapole@gmail.com>
2022-07-29 23:47:24 +0800

    Initial Commit

---
+++ MyFrame.java
@@ -0,0 +1,16 @@
+package DigitalClock;
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+
+public class MyFrame extends JFrame{
+    
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Digital Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350,200);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
