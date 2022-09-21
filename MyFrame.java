561694de645134c2b6b647f78658ffbed3c45e2b
Kenneth Apole <kennethapole@gmail.com>
2022-09-21 18:12:41 +0800

    Completed the code
    Added time, day, and date to an empty JFrame using SimepleDateFormat, Calendar, and JLabel

--- MyFrame.java
+++ MyFrame.java
@@ -1,16 +1,64 @@
 package DigitalClock;
 
-import javax.swing.JFrame;
-import java.awt.FlowLayout;
+import java.awt.*;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
 
 public class MyFrame extends JFrame{
     
+    SimpleDateFormat timeFormat, dayFormat, dateFormat;
+    JLabel timeLabel, dayLabel, dateLabel;
+    String time, day, date;
+
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Digital Clock");
         this.setLayout(new FlowLayout());
         this.setSize(350,200);
         this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+
+        timeLabel = new JLabel();
+        time = timeFormat.format(Calendar.getInstance().getTime());
+
+        timeLabel.setText(time);
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Ink Free", Font.PLAIN, 35));
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Ink Free", Font.PLAIN, 25));
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
         this.setVisible(true);
+
+        setTime();
+    }
+
+    public void setTime(){
+        while(true){
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Basic Java Pracrice
+# Basic Java practice for showing a Simple Digital Clock 
 
-Show a simple digital clock using Java
+Show a simple digital clock consisting of time, day of the week, and date, using Java.
